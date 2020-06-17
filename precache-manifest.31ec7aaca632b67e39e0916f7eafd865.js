a8737eb9742bfee19f0c99f066c8b86030a3e44e
last_light <pharshiv10@gmail.com>
2020-06-17 16:54:24 +0530

    Updates

---
+++ precache-manifest.31ec7aaca632b67e39e0916f7eafd865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31f5fe39c4977887042692fd308d3ab",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "29c35a8b496168a55d37",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c35a8b496168a55d37",
+    "url": "/react-tutorial/static/js/main.383af326.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
